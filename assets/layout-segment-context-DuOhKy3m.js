3058eb2fa4d14fe1edaf107e7ec3129dfb2a0c78
Huiyu Chen <chenhuiyu1997@gmail.com>
2026-07-29 15:31:37 +0800

    Publish site from source 0992e4f

---
+++ assets/layout-segment-context-DuOhKy3m.js
@@ -0,0 +1,1 @@
+import{r as e}from"./rolldown-runtime-S-ySWqyJ.js";import{i as t}from"./framework-DjPHiq1u.js";import{t as n}from"./index-BxhDq--T.js";var r=e(t(),1);function i({segmentMap:e,children:t}){let i=n();return i?(0,r.createElement)(i.Provider,{value:e},t):t}export{i as LayoutSegmentProvider};
